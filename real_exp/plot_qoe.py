4c4f48bc7dea5b31b3f1f1d89efe24190946befe
Paul Crews <ptcrews.jr@gmail.com>
2018-06-03 21:55:01 -0700

    Fixed an error with the way rebuffer penalty is computed

--- real_exp/plot_qoe.py
+++ real_exp/plot_qoe.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 RESULTS_FOLDER = './results/'
-SCHEMES = ['RL', 'robustMPC', 'Bola']
-TESTS = ['mahimahi-LTE', 'mahimahi-3G']
+SCHEMES = ['BOLA', 'robustMPC', 'RL']
+TESTS = ['Verizon_LTE', 'International_Link', 'Stanford_Visitor']
 BITS_IN_BYTE = 8.0
 MILLISEC_IN_SEC = 1000.0
 M_IN_B = 1000000.0
@@ -16,7 +16,7 @@ REBUF_PENALTY = 4.3
 # QoE = sum(q(R_n)) - u*sum(T_n) - sum(q(R_n) - q(R_n+1))
 def main():
 
-    colorsList = ['red', 'blue', 'orange']
+    colorsList = ['orange', 'blue', 'red']
     time_all = {}
     bit_rate_all = {}
     buff_all = {}
@@ -62,14 +62,14 @@ def main():
                     break
                 time_ms.append(float(parse[0]))
                 bit_rate.append(int(parse[1]))
-                buff.append(float(parse[2]))
+                buff.append(float(parse[3]))
                 bw.append(float(parse[4]) / float(parse[5]) * BITS_IN_BYTE * MILLISEC_IN_SEC / M_IN_B)
                 reward.append(float(parse[6]))
 
         # Compute QoE metric
         # QoE = sum(q(R_n)) - u*sum(T_n) - sum(q(R_n) - q(R_n+1))
         bitrate_sum = np.sum(bit_rate) / 1000.0
-        rebuffer_sum = np.sum(buff) / 1000.0
+        rebuffer_sum = np.sum(buff)
         bitrate_diff_sum = 0
         for j in range(len(bit_rate)-1):
             bitrate_diff_sum += abs(bit_rate[j] - bit_rate[j+1])
@@ -127,7 +127,7 @@ def main():
     # We assume the tests are in the correct order...
     x_tick_labels = tuple(TESTS)
     n_tests = len(TESTS)
-    offset = 0.4
+    offset = 0.25
     ind = np.arange(offset, n_tests + offset)
     plt.xticks(ind, x_tick_labels)
     plt.legend(plots_to_label, label_names)
