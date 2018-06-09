15f20d73af34fb5687bd5cf53864e994890e55ca
Paul Crews <ptcrews.jr@gmail.com>
2018-06-09 14:53:34 -0700

    Changed run_100_round_robin test

--- real_exp/run_100_round_robin.sh
+++ real_exp/run_100_round_robin.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-FILE_PATH="old_results/Public_Wifi"
+FILE_PATH="old_results/Stanford_Visitor"
 
 mkdir old_results
 mkdir $FILE_PATH
 
-for i in {0..120}
+for i in {0..99}
 do
     python2 ./real_world_run_video.py BOLA 480 1
-    mv ./results/log_BOLA_1 ./$FILE_PATH/log_BOLA_$i
+    mv ./results/log_BOLA_1 ./$FILE_PATH/Stanford_Visitor_log_BOLA_$i
     fuser -k -n tcp 8335
     python2 ./real_world_run_video.py RL 480 1
-    mv ./results/log_RL_1 ./$FILE_PATH/log_RL_$i
+    mv ./results/log_RL_1 ./$FILE_PATH/Stanford_Visitor_log_RL_$i
     fuser -k -n tcp 8333
     python2 ./real_world_run_video.py robustMPC 480 1
-    mv ./results/log_robustMPC_1 ./$FILE_PATH/log_robustMPC_$i
+    mv ./results/log_robustMPC_1 ./$FILE_PATH/Stanford_Visitor_log_robustMPC_$i
     fuser -k -n tcp 8334
 done
